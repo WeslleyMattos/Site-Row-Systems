d4fa86f3b68deb7819340a8262a7d3c1a78d7ab3
weslley mattos <paocomovoxs@hotmail.com>
2023-08-21 16:57:58 -0300

    Organizações de pastas e modificação do css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
-window.addEventListener('scroll', function() {
-    var nav = document.querySelector('nav');
-    if (window.scrollY > 0) {
-        nav.classList.add('visible', 'fixed');
-        nav.classList.remove('hidden');
-    } else {
-        nav.classList.remove('visible', 'fixed');
-        nav.classList.add('hidden');
-    }
-});
+// window.addEventListener('scroll', function() {
+//     var nav = document.querySelector('nav');
+//     if (window.scrollY > 0) {
+//         nav.classList.add('visible', 'fixed');
+//         nav.classList.remove('hidden');
+//     } else {
+//         nav.classList.remove('visible', 'fixed');
+//         nav.classList.add('hidden');
+//     }
+// });
