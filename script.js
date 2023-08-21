6bbe04829487ce2030ced89974d778214c44fdf7
Weslley Mattos <paocomovoxs@hotmail.com>
2023-08-21 12:48:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+    if (window.scrollY > 0) {
+        nav.classList.add('visible', 'fixed');
+        nav.classList.remove('hidden');
+    } else {
+        nav.classList.remove('visible', 'fixed');
+        nav.classList.add('hidden');
+    }
+});
