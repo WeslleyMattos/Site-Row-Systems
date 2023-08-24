652b22cecf7f3064dfbc8a6de9831d2564cc0980
weslley mattos <paocomovoxs@hotmail.com>
2023-08-24 16:42:08 -0300

    começando as page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
-
-// window.addEventListener('scroll', function() {
-//     var nav = document.querySelector('nav');
-//     if (window.scrollY > 0) {
-//         nav.classList.add('visible', 'fixed');
-//         nav.classList.remove('hidden');
-//     } else {
-//         nav.classList.remove('visible', 'fixed');
-//         nav.classList.add('hidden');
-//     }
-// });
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
+  );
+  mobileNavbar.init();
+  
